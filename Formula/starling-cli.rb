17768b0674a36444298a34be38080bcf4d0048a3
Bill Glover <bill@billglover.co.uk>
2018-07-15 12:49:58 +0100

    Brew formula update for starling-cli version 0.0.5

--- Formula/starling-cli.rb
+++ Formula/starling-cli.rb
@@ -1,9 +1,9 @@
 class StarlingCli < Formula
   desc ""
   homepage ""
-  url "https://github.com/billglover/starling-cli/releases/download/0.0.4/starling-cli_0.0.4_macOS_64-bit.tar.gz"
-  version "0.0.4"
-  sha256 "70dd8da7544226b10c0d26db59e9a4682e707b1f8179d6e409acbe224453ca56"
+  url "https://github.com/billglover/starling-cli/releases/download/0.0.5/starling-cli_0.0.5_macOS_64-bit.tar.gz"
+  version "0.0.5"
+  sha256 "86f72e00a6acdf783fcbbc22604534dd907bc9626b024889529836520be01e33"
 
   def install
     bin.install "starling-cli"
