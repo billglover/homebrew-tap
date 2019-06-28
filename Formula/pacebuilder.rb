9c24149c9aac70c17b6e243f381a667586f497c0
Bill Glover <bglover@pivotal.io>
2019-06-28 13:50:34 +0100

    Add PACE builder foruma

---
+++ Formula/pacebuilder.rb
@@ -0,0 +1,15 @@
+class PaceBuilder < Formula
+  homepage 'https://github.com/Pivotal-Field-Engineering/pace-builder'
+  version "1.5"
+  url "https://github.com/Pivotal-Field-Engineering/pace-builder/releases/download/v#{version}/pace-darwin"
+  sha256 "906b79cb00fe1773f1f633695b760a64a88f781077d9ea918385bb6553d5ab3b"
+
+  def install
+    mv "pace-darwin", "pace"
+    bin.install "pace"
+  end
+
+  test do
+    system "#{bin}/pace"
+  end
+end
